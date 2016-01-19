959335d23df9e2276bab792a74939858f9948236
zdgeorgiev <zddavko@gmail.com>
2016-01-19 10:08:46 +0200

    Refactored

--- src/main/java/com/bet/manager/commons/FootballResult.java
+++ src/main/java/com/bet/manager/commons/FootballResult.java
@@ -1,41 +1,10 @@
 package com.bet.manager.commons;
 
-import org.jsoup.helper.StringUtil;
-
 public class FootballResult extends Result {
 
     private static final String SCORE_DELIMITER = "-";
 
     public FootballResult(String firstTeam, String secondTeam) {
         super(firstTeam, secondTeam, SCORE_DELIMITER);
-    }
-
-    @Override public String getScore() {
-
-        if (StringUtil.isBlank(score)) {
-            return ResultMessages.UNKNOWN_RESULT;
-        }
-
-        return score;
-    }
-
-    @Override
-    public String getWinner() {
-
-        if (getScore().equals(ResultMessages.UNKNOWN_RESULT)) {
-            return ResultMessages.UNKNOWN_WINNER;
-        }
-
-        String[] tokens = this.score.split(this.delimiter);
-        Integer homeTeamGoals = Integer.parseInt(tokens[0]);
-        Integer awayTeamGoals = Integer.parseInt(tokens[1]);
-
-        if (homeTeamGoals > awayTeamGoals) {
-            return homeTeam;
-        } else if (homeTeamGoals < awayTeamGoals) {
-            return awayTeam;
-        } else {
-            return ResultMessages.TIE_RESULT;
-        }
     }
 }
--- src/main/java/com/bet/manager/commons/Result.java
+++ src/main/java/com/bet/manager/commons/Result.java
@@ -1,5 +1,7 @@
 package com.bet.manager.commons;
 
+import org.jsoup.helper.StringUtil;
+
 public abstract class Result {
 
     /**
@@ -33,7 +35,31 @@ public abstract class Result {
         this.score = score;
     }
 
-    public abstract String getScore();
+    public String getScore() {
 
-    public abstract String getWinner();
+        if (StringUtil.isBlank(score)) {
+            return ResultMessages.UNKNOWN_RESULT;
+        }
+
+        return score;
+    }
+
+    public String getWinner() {
+
+        if (getScore().equals(ResultMessages.UNKNOWN_RESULT)) {
+            return ResultMessages.UNKNOWN_WINNER;
+        }
+
+        String[] tokens = this.score.split(this.delimiter);
+        Integer homeTeamGoals = Integer.parseInt(tokens[0]);
+        Integer awayTeamGoals = Integer.parseInt(tokens[1]);
+
+        if (homeTeamGoals > awayTeamGoals) {
+            return homeTeam;
+        } else if (homeTeamGoals < awayTeamGoals) {
+            return awayTeam;
+        } else {
+            return ResultMessages.TIE_RESULT;
+        }
+    }
 }
